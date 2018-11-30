39687630305fffaab1a6540b4a83c54a2e03b856
Penyo Kolev <penyo.kolev@egt-interactive.com>
2018-11-30 14:57:20 +0200

    add testng.xml

--- src/main/java/com/egtinteractive/DataStructureApp.java
+++ src/main/java/com/egtinteractive/DataStructureApp.java
@@ -1,20 +1,9 @@
 package com.egtinteractive;
 
-import com.egtinteractive.list.GenericLinkedList;
-import com.egtinteractive.list.GenericList;
-
 public class DataStructureApp {
 
     public static void main(String[] args) {
-
-	GenericList<Integer> list = new GenericLinkedList<>();
-	list.add(1);
-	list.add(2);
-	GenericList<Integer> list1 = new GenericLinkedList<>();
-	list1.add(1);
-	list1.add(2);
-	list.set(0, 5);
 	
-	System.out.println(list.equals(list1));
+	
     }
 }
